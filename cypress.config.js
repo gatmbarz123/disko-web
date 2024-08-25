e89d6c14c2ea797b09735d87d037efb63f121d35
amirmirs <130896049+amirmirs@users.noreply.github.com>
2024-08-25 14:44:44 +0300

    Update cypress.config.js to ignore support file

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-    e2e: {
-      // Folder where Cypress should look for test files
-      specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  
-      // Base URL of the application under test
-      baseUrl: 'http://localhost:3000',
-  
-      // Optionally, set the viewport size for the tests
-      viewportWidth: 1280,
-      viewportHeight: 720,
-    },
-  };
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    supportFile: false,  // Disable the support file feature since it's not needed
+  },
+};
